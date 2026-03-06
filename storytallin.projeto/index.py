745403140ac4e6fbf83f9079a818baf41a638d97
Matheus Felipe <139391288+Mathezoarxxa@users.noreply.github.com>
2026-03-06 20:36:04 -0300

    Add files via upload

---
+++ storytallin.projeto/index.py
@@ -0,0 +1,35 @@
+print('PROLOGO')
+input('precione enter para proceguir')
+print('Esta é a historia de um dos maiores cavaleiros que já ousou pisar na terra de ALGUM LUGAR DISTANTE, pois oa raiar do sol no seu ponto mais alto do dia GLEM (Protagonitsa) se torna o cavaleiro, então a hora vem e ele pega:')
+input('precione enter para proceguir')
+print('1) Espada')
+print('2) Armadura')
+print('3) Cama')
+
+res= input('escolha uma resposta')
+if res == '1':
+    print('A espada? uhhh bom não é exatamente uma espada está mais para um cabo de vassoura sem a cabeça, se bem que uma vassoura tem um formato similar, não é o ideal para um grande cavaleiro mas deve servir, ahen continuando com sua ehhh, SEU item ele desse as escadas para o dia em que me foi prometido ver um cavaleiro;')
+    input('precione enter para proceguir')
+    print('Descendo as escadas conseguimos ver o nosso cavaleiro')
+    input('precione enter para proceguir')
+    print(' se arrumando para o grande dia em que ele se tornara ucavaleiro, e para isso ele deverá completar a ultima tarefa designadapelo campo de treinamento para cavaleiros iniciantes, e com tudoarrumado lá se vai ele até o centro do mercado onde o seu destino epara mostrar ao mundo a sua grandiosa história de como ele se tornou um cavaleiro')
+
+elif res == '2':
+    print('A armadura? hmm Glenn meu querido isso está mais para pano de chão em comparação, “Glenn resmunga claramente irritado com ocomentário” , oh perdão eu não queria ofender a UNICA peça de roupaque você tem disponível, “Glenn ainda mais irritado joga a roupa no chãodemonstrando sua ira” , está bem está bem eu não falo mais sobre, uhhisso, que tal descermos as escadas depois de você se trocar? temos umlongo dia pela frente afinal, “em concordância os dois dessem asescadas”')
+    input('precione enter para proceguir')
+    print('Descendo as escadas conseguimos ver o nosso cavaleiro')
+    input('precione enter para proceguir')
+    print('inde se ver no espelho de novo, “Glenn o ignora virando a cabeça” ,ah mas o nosso futuro cavaleiro com certeza não se esqueceu  de escovar os dentes, “ao mesmo tempo Glenn pega a escova e começaa escovar os dentes” , a passar perfume, “Glenn então pega e chacoalhaalgumas flores ao redor do corpo” , VER A HORA E PERCEBER QUE JÁPASSOU UMA HORA NESTA PALHAÇADA!!! , “com o grito na cabeça Glenncai no chão” , VAMOS LOGO QUE O DIA  NÃO PODE ACABAR ANTES DEVOCÊ SE TORNAR UM CAVALEIRO!!! , “depois de recobrar a razão Glennse levanta e sai correndo pela porta” , finalmente não aguentava maisver tamanha demora, ... , é melhor eu seguir ele para eu não o perder;')
+elif res == '3':
+    print('E então ele se levanta da cama e, ... , e , ... , Glenn? , ... , Glenn?! , ...suspira* Glenn já passou da hora de acordar, ... , levante-se Glenn temosaté o meio dia para a profecia se cumprir, ... , suspira* eu acho queninguém vai comer esta deliciosa batata doc-, “Antes do narradorterminar de falar a cama se revira e se debate e num piscar de olhos  Glenn se arrumou e desceu as escadas para a sala de jantar derrubandotudo pela frente” , meu deus , “exclama o narrador” , bom pelo menosele levantou da cama agora o problema é conseguir a batata que eumenti sobre;')
+    input('precione enter para proceguir')
+    print('Descendo as escadas conseguimos ver o nosso cavaleiro')
+    input('precione enter para proceguir')
+    print('futuro cavaleiro?, ... , Glenn cadê você? , ... BAM, oh não esse barulho veio da cozinha ele ainda está achando que eu tinha preparado uma batata doce para ele, “o narrador começa a andar em círculos” , e agora e agora como eu pude mentir para ele sobre a comida favorita dele, “e então com um estalar de dedos” , é isso como eu não percebi isso antes OH GLENN !!, “ao som do chamado, Glenn aparece igual a um cachorro procurando por comida” , Glenn infelismente a batata não está aqui , “desapontado ele vai para as escada com o intuito de voltar paracama” , MAS ESPERE eu não menti para você , certamente ela não estáaqui porem ela vai está no centro do mercado e nos temos queconquistar o que é seu por destino, “animado pela batata ele sai embusca do item” , espere por mim, “vai o narrador atras de Glenn”;')
+print('CONTINUA')
+
+
+
+    
+
+
